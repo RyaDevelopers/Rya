99365341e4378e6fcf9f3b4e7453c225a344ebd2
HanSolo <HanSolo@example.com>
2018-03-17 21:13:17 +0200

    fix undo

--- src/java/nxt/TransactionType.java
+++ src/java/nxt/TransactionType.java
@@ -406,7 +406,7 @@ public abstract class TransactionType {
 			if (getSubtype() == TransactionType.SUBTYPE_LOAN_GIVE_LOAN) {
 				Attachment.Loan attachment = (Attachment.Loan) transaction.getAttachment();
 				Logger.logDebugMessage("undoAttachmentUnconfirmed LOAN_GIVE amount " + String.valueOf(attachment.getLoanAmount()));
-				senderAccount.addToTrustBalance(0, -getTrustNeededForLoan(attachment.getLoanAmount()));
+				senderAccount.addToTrustBalance(-getTrustNeededForLoan(attachment.getLoanAmount()), 0);
 			} else {
 				Attachment.PayBackLoan attachment = (Attachment.PayBackLoan) transaction.getAttachment();
 				AccountLoan accountLoan = AccountLoan.GetLoan(attachment.getLoanId());
